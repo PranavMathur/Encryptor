3187f90eaee21556f73bd88ea7541ddd905b67b7
Pranav Mathur <pranavmathur99@gmail.com>
2016-02-08 20:17:17 -0800

    Add initial main

--- src/net/pranavmathur/encryptor/Main.java
+++ src/net/pranavmathur/encryptor/Main.java
@@ -1,9 +1,28 @@
 package net.pranavmathur.encryptor;
 
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+
 public class Main {
 
 	public static void main(String[] args) {
-		
+		Option passphrase = new Option("p", "passphrase", true, "use the given passphrase");
+		Options options = new Options();
+		options.addOption(passphrase);
+		CommandLineParser parser = new DefaultParser();
+		CommandLine line;
+		try {
+			line = parser.parse(options,  args);
+			System.out.println(line.hasOption("p"));
+		} catch (ParseException e) {
+			HelpFormatter formatter = new HelpFormatter();
+			formatter.printHelp("Encryptor", options);
+		}
 	}
 
 }
