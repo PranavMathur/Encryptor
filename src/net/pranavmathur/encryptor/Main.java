8fea63327ff2fcc8a0d87ba3b63c655f1d62f2f8
Pranav Mathur <pranavmathur99@gmail.com>
2016-02-12 17:56:46 -0800

    Implement XOR encryption

--- src/net/pranavmathur/encryptor/Main.java
+++ src/net/pranavmathur/encryptor/Main.java
@@ -1,6 +1,8 @@
 package net.pranavmathur.encryptor;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -33,10 +35,12 @@ public class Main {
 	}
 	
 	private static void interpretArguments(CommandLine line) throws ParseException {
-		String passphrase = (String) line.getParsedOptionValue("p");
-		if (passphrase == null)
-			passphrase = new String(System.console().readPassword("Enter a passphrase: "));
+		String optionalPassphrase = (String) line.getParsedOptionValue("p");
+		final String passphrase = optionalPassphrase != null ? 
+				optionalPassphrase : 
+					new String(System.console().readPassword("Enter a passphrase: "));
 		List<File> files = getFiles(line.getArgs());
+		files.stream().forEach(file -> encryptFile(file, passphrase));
 	}
 	
 	private static List<File> getFiles(String[] args) {
@@ -45,6 +49,45 @@ public class Main {
 			files.add(new File(s));
 		}
 		return files;
+	}
+	
+	private static boolean encryptFile(File file, String passphrase) {
+		if (file.isDirectory()) {
+			encryptDirectory(file, passphrase);
+		} else {
+			byte[] passBytes = passphrase.getBytes();
+			byte[] fileBytes;
+			try {
+				fileBytes = Files.readAllBytes(file.toPath());
+			} catch (IOException e) {
+				e.printStackTrace();
+				return false;
+			}
+			byte[] encryptedBytes = encryptBytes(fileBytes, passBytes);
+			try {
+				Files.write(file.toPath(), encryptedBytes);
+			} catch (IOException e) {
+				e.printStackTrace();
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	private static void encryptDirectory(File dir, String passphrase) {
+		for (File file : dir.listFiles()) {
+			encryptFile(file, passphrase);
+		}
+	}
+	
+	private static byte[] encryptBytes(byte[] fileBytes, byte[] passBytes) {
+		if (fileBytes.length == 0 || passBytes.length == 0)
+			return fileBytes;
+		byte[] encryptedBytes = new byte[fileBytes.length];
+		for (int i = 0; i < encryptedBytes.length; i++) {
+			encryptedBytes[i] = (byte) (fileBytes[i] ^ passBytes[i % passBytes.length]);
+		}
+		return encryptedBytes;
 	}
 
 }
